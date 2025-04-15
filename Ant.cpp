8dac90b97160770b896de62e36088d5d91834d2f
SONJIWU28 <kipo30875@gmail.com>
2025-04-15 21:35:19 +0300

    Add files via upload

---
+++ Ant.cpp
@@ -0,0 +1,167 @@
+﻿#include "Ant.h"
+#include "Anthill.h"
+
+void Ant::upd_role() {
+    switch (role_id) {
+    case 0: { role = roles[role_id++]; upd_color(); return; }
+    case 1: {
+        role_id = rand() % 2 + 2;
+        role = roles[role_id];
+        upd_color();
+        return;
+    }
+    case 2:case 3: {
+        if (max_hp <= 400) {
+            role_id = 4;
+            role = roles[role_id];
+        }
+        else {
+            role_id = 5;
+            role = roles[role_id];
+        }
+        upd_color();
+        return;
+    }
+    case 4:case 5: {
+        role_id = 6;
+        role = roles[role_id];
+        upd_color();
+        return;
+    }
+    case 6: {
+        role_id = -1;
+        hp = 0;
+        visible = 0;
+        upd_color();
+        return;
+    }
+    }
+}
+
+void Ant::move() {
+    if (hp <= 0) return;
+    if (has_target) {
+        float dx = target.x - pos.x;
+        float dy = target.y - pos.y;
+        float dist = sqrt(dx * dx + dy * dy);
+        if (dist > 5.f) {
+            velocity.x = (dx / dist) * ant_speed;
+            velocity.y = (dy / dist) * ant_speed;
+        }
+        else {
+            pos.x = target.x;
+            pos.y = target.y;
+            velocity.x = 0;
+            velocity.y = 0;
+            if (inventory != no_res && !pos.in_anthill()) {
+                set_target(Position(window_width / 2, window_height / 2));
+                going_home = true;
+            }
+            else if (inventory != no_res && pos.in_anthill()) {
+                if (inventory == food) Anthill::add_food();
+                else if (inventory == stick) Anthill::add_stick();
+                inventory = no_res;
+                clear_target();
+                going_home = false;
+            }
+            else has_target = false;
+        }
+        pos.x += velocity.x;
+        pos.y += velocity.y;
+        shape.setPosition(pos.x, pos.y);
+    }
+    else {
+        if (role_id == 0) return;
+        if (role_id != 1) {
+            if (age % velocity_changing_period == 0) set_velocity(randomise_velocity() * ant_speed, randomise_velocity() * ant_speed);
+            if (pos.x + velocity.x < 0 || pos.x + velocity.x > window_width) velocity.x = -velocity.x;
+            if (pos.y + velocity.y < 0 || pos.y + velocity.y > window_height) velocity.y = -velocity.y;
+        }
+        else {
+            if (age % (velocity_changing_period * 2) == 0) set_velocity(randomise_velocity() * ant_speed, randomise_velocity() * ant_speed);
+            if (pos.x + velocity.x < window_width / 2 - 2 * start_radius || pos.x + velocity.x > window_width / 2 + 2 * start_radius)velocity.x = -velocity.x;
+            if (pos.y + velocity.y < window_height / 2 - 2 * start_radius || pos.y + velocity.y > window_height / 2 + 2 * start_radius) velocity.y = -velocity.y;
+        }
+        pos.x += velocity.x;
+        pos.y += velocity.y;
+        shape.setPosition(pos.x, pos.y);
+    }
+}
+
+void Ant::look_around(std::vector<Resource>& resources) {
+    if (hp <= 0) return;
+    if (has_target || inventory != no_res || going_home) return;
+    for (auto& res : resources) {
+        if (!res.is_visible()) continue;
+        float dx = res.get_posit().x - pos.x;
+        float dy = res.get_posit().y - pos.y;
+        float dist = std::sqrt(dx * dx + dy * dy);
+        if (dist < radius_vision) {
+            if ((res.get_type() == food && role_id == 2) || (res.get_type() == stick && role_id == 3)) {// добавить трупы и мусор
+                set_inventory(res.get_type());
+                set_target(res.get_posit());
+                res.set_invisible();
+                break;
+            }
+        }
+    }
+}
+
+CircleShape Ant::get_vision_circle() const {
+    CircleShape vision(radius_vision);
+    vision.setOrigin(radius_vision, radius_vision); // центр круга
+    vision.setPosition(pos.x, pos.y);
+    vision.setFillColor(sf::Color(255, 255, 255, 20));  // белый, почти прозрачный
+    vision.setOutlineColor(sf::Color(0, 0, 255, 20)); // полупрозрачный контур
+    vision.setOutlineThickness(1.f);
+    return vision;
+}
+
+bool Ant::pick(Resource& res) {
+    if (!res.is_visible()) return 0;
+    if ((res.get_type() == food && role_id == 3) || (res.get_type() == stick && role_id == 2) || ((res.get_type() == body || res.get_type() == trash) && role_id == 6)) return 1;
+}
+
+void Ant::upd_color()
+{
+    if (hp <= 0 || already_dead) {
+        shape.setFillColor(Color(134, 138, 142));
+        return;
+    }
+    switch (role_id) {
+    case 0: { shape.setFillColor(Color::White); return; }
+    case 1: { shape.setFillColor(Color(255, 102, 178)); return; }
+    case 2: { shape.setFillColor(Color(255, 128, 0)); return; }
+    case 3: { shape.setFillColor(Color::Yellow); return; }
+    case 4: { shape.setFillColor(Color::Black); return; }
+    case 5: { shape.setFillColor(Color(0, 0, 204)); return; }
+    case 6: { shape.setFillColor(Color(102, 51, 0)); return; }
+    }
+}
+
+void Ant::dead(vector<Resource>& resources)
+{
+    if (already_dead) return;
+    Resource res(body, small);
+    res.set_posit(pos.x, pos.y);
+    res.set_color(body);
+    res.set_shape_size(small);
+    resources.push_back(res);
+    hp = 0;
+    velocity.x = 0;
+    velocity.y = 0;
+    has_target = false;
+    inventory = no_res;
+    already_dead = true;
+
+}
+
+float randomise_velocity()
+{
+    int n = rand() % 3;
+    switch (n) {
+    case 0: return -1.0f;
+    case 1: return 0.0f;
+    case 2: return 1.0f;
+    }
+}
---
+++ Anthill.cpp
@@ -0,0 +1,96 @@
+﻿#include "Anthill.h"
+int Anthill::food_count = 0;
+int Anthill::stick_count = 0;
+
+Anthill::Anthill() :lvl(1), rad(start_radius) {
+    shape.setRadius(rad);
+    pos.x = window_width / 2 - start_radius;
+    pos.y = window_height / 2 - start_radius;
+    shape.setPosition(Vector2f(pos.x, pos.y));
+    food_count = start_food_limit / 2;
+    stick_count = (int)(start_stick_for_update * 0.75);
+    default_count();
+    max_ants = start_max_ant_count;
+    sticks_for_upd = start_stick_for_update;
+    max_food = start_food_limit;
+    shape.setFillColor(Color(115, 66, 34));
+}
+
+void Anthill::up_lvl() {
+    lvl++;
+    stick_count /= 2;
+    max_ants *= 1.5;
+    max_food *= 1.5;
+    sticks_for_upd *= 1.5;
+    rad *= 1.2;
+    shape.setRadius(rad);
+    pos.x = window_width / 2 - rad;
+    pos.y = window_height / 2 - rad;
+    shape.setPosition(Vector2f(pos.x, pos.y));
+}
+
+void Anthill::down_lvl() {
+    lvl--;
+    rad *= 0.8;
+    max_ants = ants;
+    max_food *= 0.5;
+    sticks_for_upd *= 0.5;
+    if (food_count > max_food) food_count = max_food;
+    if (stick_count > sticks_for_upd) stick_count = sticks_for_upd;
+    shape.setRadius(rad);
+    pos.x = window_width / 2 - rad;
+    pos.y = window_height / 2 - rad;
+    shape.setPosition(Vector2f(pos.x, pos.y));
+}
+
+void Anthill::born_baby() {
+    int x, y;
+    if (ants < max_ants) {
+        do {
+            x = rand() % window_width;
+            y = rand() % window_height;
+        } while ((x < window_width / 2 - start_radius / 2) || (x > window_width / 2 + start_radius / 2) ||
+            (y < window_height / 2 - start_radius / 2) || (y > window_height / 2 + start_radius / 2));
+        colony.emplace_back(x, y);
+    }
+}
+
+void Anthill::upd_ant_stats()
+{
+    default_count();
+    for (auto& ant : colony) {
+        ants++;
+        switch (ant.get_role()) {
+        case 0:babies++; break;
+        case 1:sitters++; break;
+        case 2:collectors++; break;
+        case 3:builders++; break;
+        case 4:soldiers++; break;
+        case 5:shepherds++; break;
+        case 6:cleaners++; break;
+        }
+    }
+}
+
+void Anthill::upd_anthill(int ticks)
+{
+    upd_ant_stats();
+    //if (ticks % feeding_period == 0) feeding();
+    if (food_count > food_for_born && ticks % min_born_period == 0) {
+        born_baby();
+        food_count -= food_for_born;
+    }
+    if (stick_count == sticks_for_upd) up_lvl();
+    else if (stick_count <= 0.25 * sticks_for_upd && ticks % min_downgrade_period == 0) down_lvl();
+}
+
+void Anthill::hunger() {
+    int ant_count = colony.size();
+    if (food_count < ant_count) {
+        for (auto& ant : colony) {
+            int new_hp = ant.get_hp() - hungry_damage;
+            ant.set_hp(new_hp);
+        }
+    }
+    else food_count -= ant_count;
+}
